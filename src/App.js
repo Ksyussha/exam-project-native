bc096c59678a4bd8453cf5e0c24c0eb4b9ac3de0
Ksyussha <100123991+Ksyussha@users.noreply.github.com>
2022-10-05 17:52:16 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Main from "./components/Main";
+import Museums from "./components/MuseumList";
+
+const Stack = createStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name="MuseumScreen" component={Museums} />
+
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{ title: "Hello! my name is Museum Guide" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+// import User from "./components/user";
